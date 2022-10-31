4df5ffc12c60489b8f14c2f25c1574d4978b411f
masud001 <madhnagar@gmail.com>
2022-11-01 01:00:51 +0600

    resones page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './Components/Hero/Hero';
 import Programs from './Components/programs/Programs';
+import Reasons from './Components/Reasons/Reasons';
 function App() {
 	return (
 		<div className='App'>
 			<Hero />
 			<Programs />
+			<Reasons />
 		</div>
 	);
 }
---
+++ src/Components/Reasons/Reasons.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import image1 from '../../assets/image1.png';
+import image2 from '../../assets/image2.png';
+import image3 from '../../assets/image3.png';
+import image4 from '../../assets/image4.png';
+import nb from '../../assets/nb.png';
+import adidas from '../../assets/adidas.png';
+import nike from '../../assets/nike.png';
+import tick from '../../assets/tick.png';
+import './Reasons.css';
+
+const Reasons = () => {
+	return (
+		<div className='reasons' id='reasons'>
+			<div className='reasons-left'>
+				<img src={image1} alt='' />
+				<img src={image2} alt='' />
+				<img src={image3} alt='' />
+				<img src={image4} alt='' />
+			</div>
+			<div className='reasons-right'>
+				<span>some reasons</span>
+				<div className=''>
+					<span className='stroke-text'>why </span>
+					<span>choose us ?</span>
+				</div>
+				<div className='details-right'>
+					<div className=''>
+						<img src={tick} alt='tick-icon' />
+						<span>OVER 140+ EXPERT COACHS</span>
+					</div>
+					<div className=''>
+						<img src={tick} alt='tick-icon' />
+						<span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+					</div>
+					<div className=''>
+						<img src={tick} alt='tick-icon' />
+						<span>1 FREE PROGRAM FOR NEW MEMBER</span>
+					</div>
+					<div className=''>
+						<img src={tick} alt='tick-icon' />
+						<span>RELIABLE PARTNERS</span>
+					</div>
+				</div>
+				<span style={{ color: 'var(--gray)', fontWeight: 'normal' }}>our partners</span>
+				<div className='partners'>
+					<img src={nb} alt='brand' />
+					<img src={adidas} alt='adidas brand' />
+					<img src={nike} alt='nike brand' />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Reasons;
